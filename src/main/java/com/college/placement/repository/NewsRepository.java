d80e4ed6535f353ce5e150d0793483e525bed178
Suresh Pediredla <pediredlasuresh.24.it@anits.edu.in>
2026-06-01 17:46:44 +0530

    Added authentication module and exception handling

--- src/main/java/com/college/placement/repository/NewsRepository.java
+++ src/main/java/com/college/placement/repository/NewsRepository.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
     Page<News> findByCategory(String category, Pageable pageable);
+    Page<News> findByCategoryIgnoreCase(
+            String category,
+            Pageable pageable
+    );
 }
